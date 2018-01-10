5cb8b1c54e9d95c200a2af1ce63e3acd12c14699
Adam Moore <adamaoc@gmail.com>
2018-01-10 14:11:20 -0600

    added google analytics

--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -7,18 +7,18 @@
 
 	<script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
 
-  <?php //<script src="/assets/js/main.js"></script> ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9674179-19"></script>
 
     <script>
-			// var _gaq = _gaq || [];
-			// _gaq.push(['_setAccount', 'UA-9674179-2']);
-			// _gaq.push(['_trackPageview']);
-      //
-			// (function() {
-			// var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			// ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			// var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			// })();
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-9674179-19');
     </script>
+
+    <?php //<script src="/assets/js/main.js"></script> ?>
+
   </body>
 </html>
