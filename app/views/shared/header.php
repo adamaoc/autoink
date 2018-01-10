7933db4747e799e2ee0d8ed2eec239274dc8720e
Adam Moore <adamaoc@gmail.com>
2018-01-09 19:01:08 -0600

    google verification

--- app/views/shared/header.php
+++ app/views/shared/header.php
@@ -11,8 +11,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="shortcut icon" href="/assets/img/favicon.ico">
+        <meta name="google-site-verification" content="m8vkA3aG-t1oDiapsCvgfltPfawXE6Se1fPtrB_MhRw" />
 <!--
-        <meta name="google-site-verification" content="fSdVe-xL70WGQ4EdrP7dZemxzq1oYbc_GI48aFY1_0w" />
         <meta name="msvalidate.01" content="B038142E19DDC9427F7BB86F8022EE9C" />
 -->
         <link rel="stylesheet" href="/assets/styles/main.css">
